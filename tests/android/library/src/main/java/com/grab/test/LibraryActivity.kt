f5a57dc1ff7553725b420eea8291909eba7965c8
arunkumar9t2 <arunk.beece@gmail.com>
2024-04-09 04:49:19 +0800

    Fix imports

--- tests/android/library/src/main/java/com/grab/test/LibraryActivity.kt
+++ tests/android/library/src/main/java/com/grab/test/LibraryActivity.kt
@@ -2,7 +2,7 @@ package com.grab.test
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.grab.test.lib.LayoutTestBinding
+import com.grab.test.lib.databinding.LayoutTestBinding
 import com.grab.test.lib.R
 
 class LibraryActivity : ComponentActivity() {
@@ -11,7 +11,7 @@ class LibraryActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = LayoutTestBinding.inflate(R.layout.layout_test)
-        setContentView(binding.view)
+        binding = LayoutTestBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
